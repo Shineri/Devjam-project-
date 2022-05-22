3afc5b4d86e1d964530d571e7ab3d2501b532764
Ritu <ritugupta22332@gmail.com>
2022-05-22 22:46:34 +0530

    commit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let searchbtn =document.querySelector('#search-btn');
 let searchbar =document.querySelector('.search-bar-container');
+let formBtn =document.querySelector('#login-btn');
+let loginform =document.querySelector('.login-form-container');
+let formClose =document.querySelector('#form-close');
+
 window.onscroll=()=>{
     searchbtn.classList.remove('fa-times');
     searchbar.classList.remove('active');
@@ -7,4 +11,12 @@ window.onscroll=()=>{
 searchbtn.addEventListener('click',()=>{
     searchbtn.classList.toggle('fa-times');
     searchbar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click',()=>{
+    loginform.classList.add('active');
+});
+
+formClose.addEventListener('click',()=>{
+    loginform.classList.remove('active');
 });
