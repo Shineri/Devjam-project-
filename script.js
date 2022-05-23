1659e35c3dedb5144472a5c337f4a153a3d7cbdb
Ritu <ritugupta22332@gmail.com>
2022-05-23 17:58:27 +0530

    commit

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let searchbar =document.querySelector('.search-bar-container');
 let formBtn =document.querySelector('#login-btn');
 let loginform =document.querySelector('.login-form-container');
 let formClose =document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbar= document.querySelector('.navbar');
 
 
 let videobtn=document.querySelectorAll('.vid-btn');
@@ -10,7 +12,15 @@ let videobtn=document.querySelectorAll('.vid-btn');
 window.onscroll=()=>{
     searchbtn.classList.remove('fa-times');
     searchbar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginform.classList.remove('active');
 }
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
 searchbtn.addEventListener('click',()=>{
     searchbtn.classList.toggle('fa-times');
     searchbar.classList.toggle('active');
